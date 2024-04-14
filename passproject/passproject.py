02352dd40868099057ba5f419f4b139530588dbc
Daesun Windows <deson_m@icloud.com>
2024-04-14 18:40:52 +0500

    wip

--- passproject/passproject.py
+++ passproject/passproject.py
@@ -2,7 +2,7 @@ import reflex as rx
 import hashlib as hashf
 # dont know how to set up reflex on my desktop
 
-bg = "#1D2330"
+bg = "gray"
 
 class State(rx.State):
     name: str
@@ -15,57 +15,76 @@ class State(rx.State):
         self.passwords[self.site] = hashf.sha512(self.site.encode()).hexdigest()[:4] + hashf.sha512(self.name.encode()).hexdigest()[:4] + hashf.sha512(self.masterpass.encode()).hexdigest()[:4]
 
 def index():
-    return rx.hstack(
-        rx.hstack(
-            rx.text("ㅤㅤ"),
-            rx.badge(
-                "Password Generator", 
-                variant="soft", 
-                color_scheme="blue",
-                bg="royalblue",
-                color="white",
-                ),
-            rx.text("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"),
-            rx.link(
-                "GitHub",
-                href="https://github.com/daesun06/projectpass",
-                color="green",
-            ),
-            rx.text("ㅤㅤ"),
-            bg="black",
-            color="white",
-            height="5vh",
-        ),
-        rx.box("", height="40vh", bg=""),
+    return rx.center(
         rx.vstack(
-            rx.text("Enter your name:"),
             rx.hstack(
-                rx.input(on_change=State.set_name, value=State.name),
-            ),
-            rx.text("Enter or create your master password:"),
-            rx.hstack(
-                rx.input(on_change=State.set_masterpass, value=State.masterpass),
-            ),
-            rx.text("Enter the name of the site:"),
-            rx.hstack(
-                rx.input(on_change=State.set_site, value=State.site),
-                rx.button(
-                    "Create",
+                rx.text("ㅤㅤ"),
+                rx.badge(
+                    "Password Generator",   
+                    variant="solid", 
                     color_scheme="green",
-                    on_click=State.create_pass,
+                    bg="green",
+                    color="white",
+                    ),
+                rx.text("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"),
+                rx.color_mode.button("Switch theme", color_scheme="green", size="1", variant="solid", bg="green"),
+                rx.link(
+                    rx.text.strong("GitHub", color_scheme="green"),
+                    href="https://github.com/daesun06/projectpass",
+                    color="green",
+                    _hover={"cursor": "pointer"},
+                ),
+                rx.text("ㅤㅤ"),
+                bg="black",
+                color="white",
+                height="5vh", trim="both"
+            ),
+            rx.box("", height="30vh", bg=""),
+            rx.center(
+                rx.hstack(
+                    rx.text("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"),
+                    rx.vstack(
+                        rx.text("Enter your name:", align="center", color_scheme="green"),
+                        rx.hstack(
+                            rx.input(on_change=State.set_name, value=State.name, color_scheme="green"),
+                        ),
+                        rx.text("Enter or create your master password:", align="center", color_scheme="green"),
+                        rx.hstack(
+                            rx.input(on_change=State.set_masterpass, value=State.masterpass, type="password", color_scheme="green"),
+                        ),
+                        rx.text("Enter the name of the site:", align="center", color_scheme="green"),
+                        rx.hstack(
+                            rx.input(on_change=State.set_site, value=State.site, color_scheme="green"),
+                            rx.button(
+                                "Create",
+                                color_scheme="green",
+                                on_click=State.create_pass,
+                            ),
+                        ),
+                    ),
+                ),
+                rx.text("ㅤㅤㅤㅤㅤㅤㅤㅤㅤ"),
+                
+                rx.box(
+                    rx.heading(
+                        State.passwords[State.site],
+                        size='8', weight="bold", align="center",
+                        color='green',
+                    ),
+                    background_color="green",
+                    border_radius="6px",
+                    width="80%",
+                    margin="16px",
+                    padding="16px",
                 ),
             ),
-            rx.heading(
-                State.passwords[State.site],
-                size='5',
-                color='goldenrod',
-            ),
         ),
-        rx.box("", height="40vh", bg="bg"),
+        rx.box("", height="50vh", bg="bg"),
         center_content=True,
         bg="bg",
-        color="white",
+        color="gray",
     )
+    
 
 app = rx.App()
 app.add_page(index)
